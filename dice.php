65fe743533a50296e8782cdc237d5be1e2fac59c
filiplyrheden <filip.lyrheden@gmail.com>
2024-09-16 15:51:18 +0200

    added if-statements to dice.php

--- dice.php
+++ dice.php
@@ -9,21 +9,51 @@ $score = $firstDice + $secondDice + $thirdDice;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dice</title>
     <style>
-        b {font-size: 20px;}
-        #score {font-size: 40px; font-weight: bold;}
+        b {
+            font-size: 20px;
+        }
+
+        #score {
+            font-size: 40px;
+            font-weight: bold;
+        }
     </style>
 </head>
+
 <body>
     <p>Dice one: <b><?= $firstDice ?></b></p>
     <p>Dice two: <b><?= $secondDice ?></b></p>
-    <p>Dice three: <b><?= $thirdDice ?></b></p> 
+    <p>Dice three: <b><?= $thirdDice ?></b></p>
     <br>
     <p id="score">Score:<?= $score ?></p>
 
+    <br>
+
+    <p>
+        <?php
+
+        if ($score > 13)
+            echo "Wow! Score of 14 points or more!"
+        ?>
+
+    </p>
+
+    <p>
+        <?php
+
+        if ($firstDice == $secondDice && $secondDice == $thirdDice)
+            echo "Three pairs!";
+
+        ?>
+
+    </p>
+
 </body>
+
 </html>
